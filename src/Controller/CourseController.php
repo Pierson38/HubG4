848a921a03be01b640cef144214b8a8e1c8efcf2
Allan <ngor.allan@gmail.com>
2023-11-14 15:19:54 +0100

    Modif Cours Détail

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Courses;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CourseController extends AbstractController
 {
@@ -19,11 +22,29 @@ class CourseController extends AbstractController
     }
 
     #[Route('/course/{id}', name: 'course_detail')]
-    public function detail(Courses $course): Response
+    public function detail(Courses $course, EntityManagerInterface $entityManager): Response
     {
+        // Récupérez le titre depuis la base de données (par exemple, le premier enregistrement ici)
+        $title = $course->getTitle();
+        $description = $course->getDescription();
+        $startat = $course->getStartAt();
+        $endat = $course->getEndAt();
+        $professor = $course->getProfessor();
+
+
+        // Vérifiez si le titre existe
+        if (!$title || !$description || !$startat || !$endat || !$professor) {
+            throw $this->createNotFoundException('Aucune donnée trouvée dans la base de données.');
+        }
         // dd($course);
         return $this->render('course/detail.html.twig', [
             'course' => $course,
+            'title' => $title,
+            'description' => $description,
+            'startat' => $startat,
+            'endat' => $endat,
+            'professor' => $professor,
         ]);
     }
+
 }
