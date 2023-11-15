d6e3e2bd6bac4d7d2f217d7213438e447e83a2b1
Obylon <115164078+Obylon@users.noreply.github.com>
2023-11-15 12:11:06 +0100

    début lbc

---
+++ src/Controller/LBCController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\LbcRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LBCController extends AbstractController
+{
+    #[Route('/lbc', name: 'app_lbc')]
+    public function index(LbcRepository $lbcRepository): Response
+    {
+       // dd($lbcRepository->findAll());
+        return $this->render('lbc/index.html.twig', [
+            'products' => $lbcRepository->findAll()
+        ]);
+    }
+}
